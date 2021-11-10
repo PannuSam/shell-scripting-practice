2aa0a054a53d179ff6fba5c08a248eb749b36f53
Saman <samanpreet.pannu1126@gmail.com>
2021-11-10 17:12:18 -0500

    User to select numbers and operator

--- calc.sh
+++ calc.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env bash
 
 echo "Hello $1"
+
+echo "Please enter the numbers"
+read a
+read b
+
+echo "Please choose the type of operation :"
+echo "1. Add"
+echo "2 Subtract"
+echo "3. Multiply"
+echo "4. Division"
