92c2fe812f7fa5ebddd2f16c13ea0336649244ab
Saman <samanpreet.pannu1126@gmail.com>
2021-11-10 19:38:36 -0500

    Added condition if wish to continue to use calculator.

--- calc.sh
+++ calc.sh
@@ -2,31 +2,47 @@
 
 echo "Hello $1"
 
-echo "Please enter the numbers"
-read a
-read b
+while [[ true ]]; do
+    echo "Please enter the numbers"
+    read a
+    read b
 
-echo "Please choose the type of operation :"
-echo "1. Add"
-echo "2. Subtract"
-echo "3. Multiply"
-echo "4. Division"
-read choose
+    echo "Please choose the type of operation :"
+    echo "1. Add"
+    echo "2. Subtract"
+    echo "3. Multiply"
+    echo "4. Division"
+    read choose
 
-case $choose in
-1)
-let result="$a+$b"
-;;
-2)
-let result="$a-$b"
-;;
-3)
-let result="$a*$b"
-;;
-4)
-let result="$a/$b"
-;;
-esac
-echo "Result value is : $result"
+    result=0
+    case $choose in
+    1)
+        let result="$a+$b"
+        ;;
+    2)
+        let result="$a-$b"
+        ;;
+    3)
+        let result="$a*$b"
+        ;;
+    4)
+        let result="$a/$b"
+        ;;
+    *)
+        echo "This operation is not supported."
+        exit 0
+        ;;
+    esac
 
+    echo "Result value is : $result"
 
+    echo "Want to continue ? yes or no"
+    read ans
+
+    if [ $ans == "no" ]; then
+        break
+    else
+        echo "You can continue.."
+    fi
+
+done
